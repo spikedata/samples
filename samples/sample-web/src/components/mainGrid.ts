5e99de1e3d85481247ff2051397624775c0a49c2
ilan <ilan@spikedata.co.za>
2022-07-25 21:28:27 +0200

    transactions

--- samples/sample-web/src/components/mainGrid.ts
+++ samples/sample-web/src/components/mainGrid.ts
@@ -1,23 +1,91 @@
 import { Grid, GridOptions } from "ag-grid-community";
 
-const columnDefs = [{ field: "make" }, { field: "model" }, { field: "price" }];
-
-const rowData = [
-  { make: "Toyota", model: "Celica", price: 35000 },
-  { make: "Ford", model: "Mondeo", price: 32000 },
-  { make: "Porsche", model: "Boxter", price: 72000 },
-];
-
-const gridOptions: GridOptions = {
-  columnDefs: columnDefs,
-  rowData: rowData,
-};
-
 // document.addEventListener("DOMContentLoaded", () => {
 //   init("#myGrid");
 // });
 
-export function init(sel) {
+export function init(sel, rows) {
+  const columnDefs = [
+    { field: "id", headerName: "#", width: 60 },
+    {
+      field: "date",
+      width: 120,
+      valueGetter(params) {
+        const d = params.data;
+        // console.log(d);
+        return d.date.substring(0, 10);
+      },
+    },
+    { field: "description", flex: 1 },
+    {
+      field: "amount",
+      width: 120,
+      cellRenderer: MedalCellRenderer,
+    },
+    /*
+    {
+      field: "balance",
+      width: 120,
+      valueGetter(params) {
+        const d = params.data;
+        // console.log(d);
+        return Rand(d.balance);
+      },
+    },
+    */
+  ];
+  const gridOptions: GridOptions = {
+    columnDefs: columnDefs,
+    rowData: rows,
+
+    defaultColDef: {
+      sortable: true,
+      // flex: 1,
+      // minWidth: 100,
+      filter: true,
+      resizable: true,
+    },
+
+    // rowClassRules: {
+    //   red: function (params) {
+    //     return params.data.amount < 0;
+    //   },
+    //   green: function (params) {
+    //     return params.data.amount > 0;
+    //   },
+    // },
+  };
+
   const gridDiv = <HTMLElement>document.querySelector(sel);
   new Grid(gridDiv, gridOptions);
+}
+
+export function Rand(num) {
+  if (num === undefined) return "";
+  return new Intl.NumberFormat("en-ZA", {
+    style: "currency",
+    currency: "ZAR",
+  }).format(num); // R 2 500,00
+}
+
+class MedalCellRenderer {
+  // init method gets the details of the cell to be renderer
+  init(params) {
+    const d = params.data;
+    const amt = Rand(d.amount);
+    this.eGui = document.createElement("span");
+    if (d.amount < 0) {
+      this.eGui.innerHTML = `<span class="font-semibold text-red-600">${amt}</span>`;
+    } else {
+      this.eGui.innerHTML = `<span class="font-semibold text-green-600">${amt}</span>`;
+    }
+  }
+
+  getGui() {
+    return this.eGui;
+  }
+
+  refresh(params) {
+    return false;
+  }
 }
--- samples/sample-web/src/views/Home.vue
+++ samples/sample-web/src/views/Home.vue
@@ -9,27 +9,24 @@
   -->
 
   <!-- auto-vertical -->
-  <div class="bg-gray-200 w-screen h-screen p-10 flex flex-col space-y-6">
+
+  <div class="bg-gray-200 w-screen h-screen p-2 flex flex-col space-y-6">
     <div class="flex space-x-2">
-      <h2
-        class="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 sm:text-4xl sm:leading-10"
-      >
+      <h2 class="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 sm:text-4xl sm:leading-10">
         Categorise
       </h2>
     </div>
 
-    <div
-      id="myGrid"
-      style="width: 600px; height: 600px"
-      class="ag-theme-alpine"
-    ></div>
+    <div id="myGrid" style="width: 800px; height: 600px" class="ag-theme-alpine"></div>
   </div>
 </template>
+
 <script setup>
+import EXAMPLES from "@spike/api-statements/examples/gen/index.js";
 import * as MainGrid from "@/components/mainGrid";
 import { onMounted } from "vue";
 
 onMounted(() => {
-  MainGrid.init("#myGrid");
+  MainGrid.init("#myGrid", EXAMPLES["pdf/success/bank-statement-normal"].result.data.transactions);
 });
 </script>
